f8e72049a87cb634b56bd4a6fd15788d9fe24e3c
tuddy <olartudor12@gmail.com>
2024-11-06 20:57:51 +0200

    Prima implementare, am facut tabelele in baza de date si controllere-le de CRUD.

---
+++ Controllers/PlayerRatingsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using sports_up_backend.Database;
+using sports_up_backend.Models;
+
+namespace sports_up_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayerRatingsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PlayerRatingsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PlayerRatings
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PlayerRating>>> GetPlayerRatings()
+        {
+            return await _context.PlayerRatings.ToListAsync();
+        }
+
+        // GET: api/PlayerRatings/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PlayerRating>> GetPlayerRating(int id)
+        {
+            var playerRating = await _context.PlayerRatings.FindAsync(id);
+
+            if (playerRating == null)
+            {
+                return NotFound();
+            }
+
+            return playerRating;
+        }
+
+        // PUT: api/PlayerRatings/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPlayerRating(int id, PlayerRating playerRating)
+        {
+            if (id != playerRating.RatingId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(playerRating).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlayerRatingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PlayerRatings
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PlayerRating>> PostPlayerRating(PlayerRating playerRating)
+        {
+            _context.PlayerRatings.Add(playerRating);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPlayerRating", new { id = playerRating.RatingId }, playerRating);
+        }
+
+        // DELETE: api/PlayerRatings/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePlayerRating(int id)
+        {
+            var playerRating = await _context.PlayerRatings.FindAsync(id);
+            if (playerRating == null)
+            {
+                return NotFound();
+            }
+
+            _context.PlayerRatings.Remove(playerRating);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PlayerRatingExists(int id)
+        {
+            return _context.PlayerRatings.Any(e => e.RatingId == id);
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace sports_up_backend.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PrefferedSports { get; set; }
+        public ICollection<Lobby> OwnedLobbies { get; } = new List<Lobby>();
+        public ICollection<LobbyPlayer> LobbyPlayers { get; } = new List<LobbyPlayer>();
+        public ICollection<PlayerRating> RatingsGiven { get; } = new List<PlayerRating>();
+        public ICollection<PlayerRating> RatingsReceived { get; } = new List<PlayerRating>();
+        public ICollection<Message> SentMessages { get; } = new List<Message>();
+    }
+}
