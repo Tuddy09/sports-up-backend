c0d10b2647ce5e3c46737205f1789d26a3fad150
912-Contiu-Mario <mario.contiu@stud.ubbcluj.ro>
2024-12-11 23:21:14 +0200

    added age and AvatarId to User and created UserProfileDTO

---
+++ Data Transfer Obejcts/UserProfileDTO.cs
@@ -0,0 +1,14 @@
+﻿using Azure.Identity;
+
+namespace sports_up_backend.Data_Transfer_Obejcts
+{
+    public class UserProfileDTO
+    {
+        public string Username { get; set; }
+        public int Age { get; set; }
+        public int AvatarId { get; set; }
+        public int TotalMatchesPlayed { get; set; }
+        public string PreferredSport { get; set; }
+        public int Rating { get; set; }
+    }
+}
