fcecd11a663ed574854afb01a02b7ad931d6cb38
Darius Lacatus <dariusandrey2003@gmail.com>
2024-12-12 22:39:37 +0200

    changed PlayerRating model and added new DTO

---
+++ Data Transfer Obejcts/PlayerRatingDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace sports_up_backend.Data_Transfer_Obejcts
+{
+    public class PlayerRatingDTO
+    {
+        public int FromUserId { get; set; }
+        public int ToUserId { get; set; }
+        public string Comment { get; set; }
+        public int Rating { get; set; }
+    }
+}
--- Models/PlayerRating.cs
+++ Models/PlayerRating.cs
@@ -6,20 +6,32 @@ namespace sports_up_backend.Models
 {
     public class PlayerRating
     {
+
+        public PlayerRating() { }
+
+        public PlayerRating(int ratedByUserId, User ratedByUser, int ratedUserId, User ratedUser, string message, int stars)
+        {
+            RatedByUserId = ratedByUserId;
+            RatedUserId = ratedUserId;
+            RatedByUser = ratedByUser;
+            RatedUser = ratedUser;
+            Message = message;
+            Stars = stars;
+        }
+
         [Key]
         public int RatingId { get; set; }
 
         public int RatedByUserId { get; set; }
         [ForeignKey("RatedByUserId")]
         [JsonIgnore]  // Ignore during serialization
-        public User RatedByUser { get; set; } = null!;
+        public User RatedByUser { get; set;  } = null!;
 
         public int RatedUserId { get; set; }
         [ForeignKey("RatedUserId")]
         [JsonIgnore]  // Ignore during serialization
-        public User RatedUser { get; set; } = null!;
-            
-        public string Category { get; set; }
+        public User RatedUser { get; set;  } = null!;
+        public string Message { get; set; }
         public int Stars { get; set; }
     }
 }
